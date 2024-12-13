6059b772dc07e5c3273cf56d51670676c10dbb7a
Diego Fornalha <diegofornalha@gmail.com>
2024-12-13 17:26:09 -0300

    melhoramentos para o build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,33 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
+import { ReactNode } from 'react'
 import './globals.css'
 import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' }
+  ]
+}
 
 export const metadata: Metadata = {
   title: {
     template: '%s | ChatGPT Clone',
     default: 'ChatGPT Clone',
   },
-  description: 'Um clone do ChatGPT usando Next.js e NextUI',
-  viewport: 'width=device-width, initial-scale=1',
-  themeColor: '#ffffff',
+  description: 'Um clone do ChatGPT usando Next.js e NextUI'
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+interface RootLayoutProps {
+  children: ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="pt-BR">
       <body className={`${inter.className} min-h-screen bg-background`}>
